862a6afca3be6280ed50af134370a5fca28fff90
João Paulo <johnowl@gmail.com>
2019-07-24 19:57:13 -0300

    Solve kotlin compiler warning.

--- src/main/kotlin/com/johnowl/toggle/server/domain/ToggleRepository.kt
+++ src/main/kotlin/com/johnowl/toggle/server/domain/ToggleRepository.kt
@@ -4,6 +4,6 @@ interface ToggleRepository {
     fun getById(toggleId: String) : FeatureToggle?
     fun add(featureToggle: FeatureToggle): FeatureToggle
     fun getAll(): List<FeatureToggle>
-    fun update(toggleWithId: FeatureToggle): FeatureToggle
+    fun update(featureToggle: FeatureToggle): FeatureToggle
     fun delete(toggleId: String)
 }
